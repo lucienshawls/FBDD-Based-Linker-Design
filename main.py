eb5f3a4f6aae0df758118c1cd707ecba07cbcced
Lucien Shaw <myxlc55@outlook.com>
2023-02-20 16:04:07 +0800

    dev: 初版，不可分段存储

---
+++ main.py
@@ -0,0 +1,110 @@
+import requests
+import re
+import json
+import csv
+import time
+
+COMPOUND_LIST_PAGE = 'http://cadd.zju.edu.cn/protacdb/browse/compound'
+COMPOUND_PAGE = 'http://cadd.zju.edu.cn/protacdb/compound/dataset=protac&id='
+TOTAL = 5388
+
+def compound(id):
+    info = {
+        'id': id,
+        'protac': {
+            'id': '',
+            'smiles': ''
+        },
+        'e3_ligand': {
+            'id': '',
+            'smiles': ''
+        },
+        'linker': {
+            'id': '',
+            'smiles': ''
+        },
+        'warhead': {
+            'id': '',
+            'smiles': ''
+        },
+        'target': '',
+    }
+    compound_page = 'http://cadd.zju.edu.cn/protacdb/compound/dataset=protac&id=%d' %(id)
+
+    cookies = {'_xsrf': '2|77ac8144|82b88bc11af2f09bfdf53c753ca8b906|1676875379',}
+    headers = {
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6',
+        'Connection': 'keep-alive',
+        'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryRJRMQVyUyjFMx6O0',
+        'DNT': '1',
+        'Origin': 'http://cadd.zju.edu.cn',
+        'Referer': compound_page,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.50',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+    data = '------WebKitFormBoundaryRJRMQVyUyjFMx6O0\r\nContent-Disposition: form-data; name="_xsrf"\r\n\r\n2|77ac8144|82b88bc11af2f09bfdf53c753ca8b906|1676875379\r\n------WebKitFormBoundaryRJRMQVyUyjFMx6O0--\r\n'
+
+    response = requests.post(
+        compound_page,
+        headers=headers,
+        verify=False,
+        cookies=cookies,
+        data=data
+    )
+    resp = response.json()
+
+    info['protac']['id'] = resp[0]['id_in_database']
+    info['protac']['smiles'] = resp[0]['smiles_canonical']
+    info['e3_ligand']['id'] = resp[0]['id_e3_ligand']
+    info['e3_ligand']['smiles'] = resp[0]['e3_ligand_canonical']
+    info['linker']['id'] = resp[0]['id_linker']
+    info['linker']['smiles'] = resp[0]['linker_canonical']
+    info['warhead']['id'] = resp[0]['id_warhead']
+    info['warhead']['smiles'] = resp[0]['warhead_canonical']
+
+    info['target'] = resp[-1]['target']
+    return info
+
+def compound_list():
+    tot = TOTAL
+    dataset = [[
+        'id',
+        'target',
+        'protac_id',
+        'protac_smiles',
+        'e3_ligand_id',
+        'e3_ligand_smiles',
+        'linker_id',
+        'linker_smiles',
+        'warhead_id',
+        'warhead_smiles',
+    ]]
+    for i in range (1,tot+1):
+        print('Processing: %d/%d' %(i,tot))
+        data = compound(i)
+        table_row = [
+            data['id'],
+            data['target'],
+            data['protac']['id'],
+            data['protac']['smiles'],
+            data['e3_ligand']['id'],
+            data['e3_ligand']['smiles'],
+            data['linker']['id'],
+            data['linker']['smiles'],
+            data['warhead']['id'],
+            data['warhead']['smiles'],
+        ]
+
+        dataset.append(table_row)
+    
+    return dataset
+
+def main():
+    table = compound_list()
+    with open('./data/protac.csv','w',encoding='utf-8-sig') as f:
+        writer = csv.writer(f)
+        writer.writerows(table)
+
+if __name__ == '__main__':
+    main()
